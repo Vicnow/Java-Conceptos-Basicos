5f9b55e007d4e1f1d293258a91bd335445b8db15
Vicnow <vchgmm_personal@outlook.com>
2023-11-27 23:15:25 -0600

    Cambios en los archivos de IntelliJ IDEA

--- src/VariablesTypes.java
+++ src/VariablesTypes.java
@@ -1,27 +1,27 @@
 public class VariablesTypes {
     public static void main(String[] args) {
-        // EL tipo de dato Interger es una clase que representa un número entero y tinene métodos para operar con él
-        Integer numberInterger = 5;
+        // El tipo de dato Integer es una clase que representa un número entero y tiene métodos para operar con él
+        Integer numberInteger = 5;
         // El tipo de dato int es un tipo de dato primitivo que representa un número entero y no tiene métodos para operar con él
         int numberPrimitive = 5;
 
-        System.out.println("Suma de variable Interger y variable int: " + (numberInterger + numberPrimitive));
+        System.out.println("Suma de variable Integer y variable int: " + (numberInteger + numberPrimitive));
 
-        // EL tipo de dato Double es una clase que representa un número decimal y tinene métodos para operar con él
+        // El tipo de dato Double es una clase que representa un número decimal y tiene métodos para operar con él
         Double numberDouble = 5.5;
         // El tipo de dato double es un tipo de dato primitivo que representa un número decimal y no tiene métodos para operar con él
         double numberPrimitiveDouble = 5.5;
 
         System.out.println("Suma de variable Double y variable double: " + (numberDouble + numberPrimitiveDouble));
 
-        // EL tipo de dato Boolean es una clase que representa un valor booleano y tinene métodos para operar con él
+        // El tipo de dato Boolean es una clase que representa un valor booleano y tiene métodos para operar con él
         Boolean booleanValue = true;
         // El tipo de dato boolean es un tipo de dato primitivo que representa un valor booleano y no tiene métodos para operar con él
         boolean booleanPrimitive = true;
 
         System.out.println("Concatenación de variable Boolean y variable boolean: " + (booleanValue && booleanPrimitive));
 
-        // EL tipo de dato String es una clase que representa una cadena de caracteres y tinene métodos para operar con él
+        // El tipo de dato String es una clase que representa una cadena de caracteres y tiene métodos para operar con él
         // No existe un tipo de dato primitivo que represente una cadena de caracteres
         String stringValue = "Hola";
 
@@ -32,6 +32,5 @@ public class VariablesTypes {
         // varValue = 5; // Esto no se puede hacer porque ya se asignó un valor de tipo String
 
         System.out.println("Variable var: " + (varValue));
-
     }
 }
