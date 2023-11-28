595371c0c914824d848687430c550b3f6f70f1ce
Vicnow <vchgmm_personal@outlook.com>
2023-11-28 00:25:31 -0600

    Sistemas númericos con ventana emergente y try-catch

--- src/UserInputs.java
+++ src/UserInputs.java
@@ -3,13 +3,14 @@ import javax.swing.*;
 public class UserInputs {
     public static void main(String[] args) {
         int decimalNumber = 0;
-
+        // El bloque try-catch permite capturar excepciones
         try {
             // El método parseInt de la clase Integer convierte una cadena de caracteres a un número entero
             decimalNumber = Integer.parseInt(
                     // El método showInputDialog de la clase JOptionPane muestra un cuadro de diálogo para ingresar un valor
                     JOptionPane.showInputDialog(null, "Ingrese un número entero")
             );
+            // NumberFormatException es una excepción que se lanza cuando se intenta convertir una cadena de caracteres a un número entero y la cadena de caracteres no es un número entero
         } catch (NumberFormatException e) {
             // El método showMessageDialog de la clase JOptionPane muestra un cuadro de diálogo con un mensaje
             JOptionPane.showMessageDialog(null, "El valor ingresado no es un número entero");
