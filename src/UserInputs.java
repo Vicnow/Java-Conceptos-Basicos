0e6f78f8f4be571450ca3370227d1880deed17d7
Vicnow <vchgmm_personal@outlook.com>
2023-11-28 00:24:16 -0600

    Sistemas númericos con ventana emergente y try-catch

--- src/UserInputs.java
+++ src/UserInputs.java
@@ -2,11 +2,19 @@ import javax.swing.*;
 
 public class UserInputs {
     public static void main(String[] args) {
-        // El método parseInt de la clase Integer convierte una cadena de caracteres a un número entero
-        int decimalNumber = Integer.parseInt(
-                // El método showInputDialog de la clase JOptionPane muestra un cuadro de diálogo para ingresar un valor
-                JOptionPane.showInputDialog(null, "Ingrese un número entero")
-        );
+        int decimalNumber = 0;
+
+        try {
+            // El método parseInt de la clase Integer convierte una cadena de caracteres a un número entero
+            decimalNumber = Integer.parseInt(
+                    // El método showInputDialog de la clase JOptionPane muestra un cuadro de diálogo para ingresar un valor
+                    JOptionPane.showInputDialog(null, "Ingrese un número entero")
+            );
+        } catch (NumberFormatException e) {
+            // El método showMessageDialog de la clase JOptionPane muestra un cuadro de diálogo con un mensaje
+            JOptionPane.showMessageDialog(null, "El valor ingresado no es un número entero");
+            System.exit(0);
+        }
 
         String message = "";
         message += "Número Decimal: " + decimalNumber + "\n";
